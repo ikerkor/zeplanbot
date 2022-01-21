2bd0cc4d6fade82400c240e04cceee8c1d31adba
etxe <ikorker@gmail.com>
2022-01-21 13:26:16 +0100

    Herrien zerrenda lortu eta hau erabili requesta egiteko beharrezko kodeak izateko.

--- inline.py
+++ inline.py
@@ -1,12 +1,35 @@
 import settings
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 from telegram.ext import CallbackContext
+import requests
+import datetime
 
 
 def inlinequery(update: Update, context: CallbackContext) -> None:
     """Handle the inline query."""
     stQuery = update.inline_query.query
-    results = []
+    # Herrien kodeak lortu
+    lstHerriak = []
+    response = requests.get("https://api.euskadi.eus/culture/events/v1.0/municipalities?_elements=20&_page=1")
+    dicJson = response.json()
+    lstHerriak += dicJson['items']
+    iPages = dicJson['totalPages']
+    for i in range(iPages-1):
+        response = requests.get(f"https://api.euskadi.eus/culture/events/v1.0/municipalities?_elements=20&_page={i+2}")
+        dicJson = response.json()
+        lstHerriak += dicJson['items']
+
+    for dicHerri in lstHerriak:
+        if dicHerri['nameEu'] == stQuery:
+            stIdHerri = dicHerri['municipalityId']
+            stIdProbintzi = dicHerri['provinceId']
+            tGaur = datetime.date.today()
+            response = requests.get(f"https://api.euskadi.eus/culture/events/v1.0/events/byMonth/{str(tGaur.year)}/{str(tGaur.month)}/byMunicipality/{stIdProbintzi}/{stIdHerri}?_elements=50&_page=1")
+            dicJson = response.json()
+            print(dicJson)
+            break
+
+'''   results = []
     results.append(
         InlineQueryResultArticle(
             id='auskalo',
@@ -18,7 +41,7 @@ def inlinequery(update: Update, context: CallbackContext) -> None:
             input_message_content=InputTextMessageContent(stQuery)
         )
     )
-    update.inline_query.answer(results)
+    update.inline_query.answer(results)'''
 
 
 '''def chosen(update: Update, context: CallbackContext) -> None:
