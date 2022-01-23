d7df755aa4846452819295a5be34926902ffdd0b
etxe <ikorker@gmail.com>
2022-01-23 21:51:57 +0100

    Herrien zerrenda orrialde batean. Gidoidun herriak.

--- inline.py
+++ inline.py
@@ -1,33 +1,46 @@
-import settings
-from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
-from telegram.ext import CallbackContext
-import requests
 import datetime
 
+import requests
+from telegram import Update
+from telegram.ext import CallbackContext
+from dateutil.relativedelta import relativedelta
 
 def inlinequery(update: Update, context: CallbackContext) -> None:
     """Handle the inline query."""
     stQuery = update.inline_query.query
     # Herrien kodeak lortu
-    lstHerriak = []
-    response = requests.get("https://api.euskadi.eus/culture/events/v1.0/municipalities?_elements=20&_page=1")
+    response = requests.get("https://api.euskadi.eus/culture/events/v1.0/municipalities?_elements=300&_page=1")
     dicJson = response.json()
-    lstHerriak += dicJson['items']
+    lstHerriak = dicJson['items']
+    '''
     iPages = dicJson['totalPages']
     for i in range(iPages-1):
         response = requests.get(f"https://api.euskadi.eus/culture/events/v1.0/municipalities?_elements=20&_page={i+2}")
         dicJson = response.json()
-        lstHerriak += dicJson['items']
+        lstHerriak += dicJson['items']'''
 
     for dicHerri in lstHerriak:
-        if dicHerri['nameEu'] == stQuery:
+        lstGidoi = dicHerri['nameEu'].split('-')
+        bGidoi = False
+        for stZati in lstGidoi:
+            if stQuery == stZati:
+                bGidoi = True
+                break
+        if bGidoi:
             stIdHerri = dicHerri['municipalityId']
             stIdProbintzi = dicHerri['provinceId']
             tGaur = datetime.date.today()
-            response = requests.get(f"https://api.euskadi.eus/culture/events/v1.0/events/byMonth/{str(tGaur.year)}/{str(tGaur.month)}/byMunicipality/{stIdProbintzi}/{stIdHerri}?_elements=50&_page=1")
-            dicJson = response.json()
-            print(dicJson)
+            tHileBarru = tGaur + relativedelta(months=1)
+            if tGaur.day == 31 and tHileBarru.day == 1:
+                tHileBarru.day = 30
+            lstEkintzak = []
+            for iHile in range(2):
+                response = requests.get(f"https://api.euskadi.eus/culture/events/v1.0/events/byMonth/{str(tGaur.year)}/{str(tGaur.month+1)}/byMunicipality/{stIdProbintzi}/{stIdHerri}?_elements=300&_page=1")
+                lstEkintzak += response.json()['items']
+                for i in range(len(lstEkintzak)):
+                    if date(lstEkintzak[i]['startDate']) >
             break
+
 
 '''   results = []
     results.append(
