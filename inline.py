1accfef7a9e5feac7e64fa3acf4c54fc7417cb19
etxe <ikorker@gmail.com>
2022-01-27 21:28:55 +0100

    Kontsulta egunetik aurrerakoa eta euskarakoak mantenduta. Inline zerrendea martxan.

--- inline.py
+++ inline.py
@@ -1,7 +1,7 @@
 import datetime
 
 import requests
-from telegram import Update
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import CallbackContext
 from dateutil.relativedelta import relativedelta
 
@@ -26,35 +26,49 @@ def inlinequery(update: Update, context: CallbackContext) -> None:
             if stQuery == stZati:
                 bGidoi = True
                 break
-        if bGidoi:
+        if bGidoi or stQuery == dicHerri['nameEu']:
             stIdHerri = dicHerri['municipalityId']
             stIdProbintzi = dicHerri['provinceId']
             tGaur = datetime.date.today()
             tHileBarru = tGaur + relativedelta(months=1)
             if tGaur.day == 31 and tHileBarru.day == 1:
-                tHileBarru.day = 30
+                tHileBarru.replace(day=28)
             lstEkintzak = []
             for iHile in range(2):
-                response = requests.get(f"https://api.euskadi.eus/culture/events/v1.0/events/byMonth/{str(tGaur.year)}/{str(tGaur.month+1)}/byMunicipality/{stIdProbintzi}/{stIdHerri}?_elements=300&_page=1")
+                response = requests.get(f"https://api.euskadi.eus/culture/events/v1.0/events/byMonth/{str(tGaur.year)}/{str(tGaur.month+iHile)}/byMunicipality/{stIdProbintzi}/{stIdHerri}?_elements=300&_page=1")
                 lstEkintzak += response.json()['items']
-                for i in range(len(lstEkintzak)):
-                    if date(lstEkintzak[i]['startDate']) >
-            break
+                lstind = []
+                # Gaur eta aurreragokoak bakarrik mantendu
+                if iHile == 0:
+                    for i in reversed(range(len(lstEkintzak))):
+                        if datetime.datetime.strptime(lstEkintzak[i]['endDate'][:-10], '%Y-%m-%d').date() >= tGaur:
+                            lstind.append(i)
+                        else:
+                            break
+                    lstEkintzak = [lstEkintzak[j] for j in lstind]
+            lstind = []
+            # Euskarazkoak bakarrik mantendu
+            for i in range(len(lstEkintzak)):
+                if lstEkintzak[i]['language'] == 'EU': # FIXME: field hori ez badauka, ez da onartuko baina arazorik ez
+                    lstind.append(i)
+            lstEkintzak = [lstEkintzak[i] for i in lstind]
 
-
-'''   results = []
-    results.append(
-        InlineQueryResultArticle(
-            id='auskalo',
-            title=stQuery,
-            description='blabla',
-            thumb_url=None,
-            thumb_width=None,
-            thumb_height=None,
-            input_message_content=InputTextMessageContent(stQuery)
-        )
-    )
-    update.inline_query.answer(results)'''
+            #Inline zerrenda sortu
+            results = []
+            for dicEkintza in lstEkintzak:
+                # TODO: ez dadila arazo izan field bat ez izatea. Tipa emotikono bidez?
+                results.append(
+                    InlineQueryResultArticle(
+                        id=dicEkintza['id'],
+                        title=dicEkintza['nameEu'],
+                        description=dicEkintza['openingHoursEu'],
+                        thumb_url=dicEkintza['images'][0]['imageUrl'],
+                        thumb_width=None,
+                        thumb_height=None,
+                        input_message_content=InputTextMessageContent('Etorri nahi?')
+                    )
+                )
+            update.inline_query.answer(results)
 
 
 '''def chosen(update: Update, context: CallbackContext) -> None:
